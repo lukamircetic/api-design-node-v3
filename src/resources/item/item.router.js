3990fcdcb3806805c79e4ba4bb9eb2aa7470ed09
Luka Mircetic <luka.mircetic@gmail.com>
2020-07-29 14:09:33 -0400

    lesson2 done

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -1,5 +1,18 @@
 import { Router } from 'express'
 
+const mockController = (req, res) => {
+  res.json({ message: 'ok' })
+}
 const router = Router()
 
+router
+  .route('/')
+  .get(mockController)
+  .post(mockController)
+
+router
+  .route('/:id')
+  .get(mockController)
+  .put(mockController)
+  .delete(mockController)
 export default router
