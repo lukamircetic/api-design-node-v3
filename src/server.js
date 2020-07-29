523594e55051dbc4b86aa925ab9d89e599354690
Luka Mircetic <luka.mircetic@gmail.com>
2020-07-29 13:13:27 -0400

    lesson1 done

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import morgan from 'morgan'
 import cors from 'cors'
 
 export const app = express()
+const router = express.Router()
 
 app.disable('x-powered-by')
 
@@ -12,4 +13,38 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = () => {}
+router.get('/me', (req, res) => {
+  res.send({ me: 'hello' })
+})
+
+router
+  .route('/cat')
+  .get()
+  .post()
+
+router
+  .route('/cat/:id')
+  .get()
+  .put()
+  .delete()
+
+app.use('/api', router)
+
+const log = (req, res, next) => {
+  console.log('logging')
+  next()
+}
+
+app.get('/data', log, (req, res) => {
+  res.send({ data: [1, 2, 3] })
+})
+
+app.post('/data', (req, res) => {
+  console.log(req.body)
+  res.send({ ok: true })
+})
+export const start = () => {
+  app.listen(3000, () => {
+    console.log('server is listening on 3000')
+  })
+}
